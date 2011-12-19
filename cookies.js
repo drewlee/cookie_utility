b01123854b6c893291792dc64bc1aaa7775ad8c0
Andrew Lee <starsthatfell@gmail.com>
2011-12-18 22:31:01 -0600

    Updated to parse cookie keys using regular expressions instead of indexOf, which is inaccurate with similarly named keys.

--- cookies.js
+++ cookies.js
@@ -1,47 +1,68 @@
 /*
- * JavaScript Cookie Utility v1.1
+ * JavaScript Cookie Utility v1.2
  *
  * Copyright (c) 2011 Andrew A. Lee
  *
  * Dual licensed under the MIT and GPL licenses, located in
  * MIT-LICENSE.txt and GPL-LICENSE.txt respectively.
  *
- * Mon Sep 05 2011 17:26:05 GMT-0500 (CDT)
+ * Sun Dec 18 2011 22:26:46 GMT-0600 (CST)
  */
-var Cookie = {
-	setCookie: function(name, value, opts){
-		var a = [escape(name) + "=" + escape(value)];
-		if(opts){
-			if(opts.expires)a.push("expires=" + opts.expires.toUTCString());
-			if(opts.path)a.push("path=" + escape(opts.path));
-			if(opts.domain)a.push("domain=" + escape(opts.domain));
-			if(opts.secure)a.push("secure");
-		}
-		document.cookie = a.join("; ");
-		if(document.cookie.indexOf(escape(name)) > -1)return true;
-		return false;
-	},
-	getCookie: function(name){
-		name += '=';
-		var c = document.cookie, pos = c.indexOf(name);
-		if(pos > -1){
-			var semi = c.indexOf(';', pos);
-			var value = c.substring(pos + name.length, semi > -1 ? semi : c.length);
-			return unescape(value);
-		}
-		return null;
-	},
-	deleteCookie: function(name, opts){
-		name = escape(name);
-		if(document.cookie.indexOf(name) > -1){
-			var a = [name + "=null", "expires=" + new Date(new Date().getTime() - (1000*60*60*24*360) - 20).toUTCString()];
-			if(opts){
-				if(opts.path)a.push("path=" + escape(opts.path));
-				if(opts.domain)a.push("domain=" + escape(opts.domain));
-			}
-			document.cookie = a.join("; ");
-			if(document.cookie.indexOf(name) < 0)return true;
-		}
-		return false;
+var Cookie = (function(){
+	function escapeRegExMeta(str){
+		str = str.replace(/[\\^[\].${}*(+)|?<>]/g, function(s){
+			return '\\'+s; 
+		});
+	
+		return str;
 	}
-};
+	
+	function getValue(key){
+		key = escapeRegExMeta(key);
+		var value = document.cookie.match(new RegExp('(^'+key+'|; '+key+')=([^;]+)'));
+		
+		if(value){value = value.pop();}
+		
+		return value;
+	}
+	
+	return {
+		set: function(name, value, opts){
+			var a = [name + "=" + escape(value)];
+			opts = opts || {};
+			
+			if('expires' in opts){a.push("expires=" + opts.expires.toUTCString())};
+			if('path' in opts){a.push("path=" + escape(opts.path))};
+			if('domain' in opts){a.push("domain=" + escape(opts.domain))};
+			if('secure' in opts){a.push("secure")};
+			
+			document.cookie = a.join("; ");
+			
+			if(getValue(name)){return true;}
+			
+			return false;
+		},
+		
+		get: function(name){
+			var value = getValue(name);
+			return !value ? false : unescape(value);
+		},
+		
+		remove: function(name, opts){
+			if(getValue(name)){
+				opts = opts || {};
+				
+				var a = [name + "=null", "expires=" + new Date(new Date().getTime() - (1000*60*60*24*30) - 1).toUTCString()];
+				
+				if('path' in opts){a.push("path=" + escape(opts.path))};
+				if('domain' in opts){a.push("domain=" + escape(opts.domain))};
+				
+				document.cookie = a.join("; ");
+				
+				if(!getValue(name)){return true;}
+			}
+			
+			return false;
+		}
+	};
+})();
